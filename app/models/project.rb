f8936d9b03d628219efa7f4b5339cf8e92fcb923
yousamaaa <usama.yousaf@devsinc.com>
2022-10-20 17:27:47 +0500

    MODEL VALIDATIONS COMPLETE

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,4 +6,6 @@ class Project < ApplicationRecord
   has_many :developers,           through: :project_enrollments, class_name: 'User'
 
   belongs_to :manager,            class_name: 'User'
+
+  validates :title, :description, presence: true
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -7,5 +7,8 @@ class Ticket < ApplicationRecord
   belongs_to :developer,  class_name: 'User', foreign_key: 'assigned_to_id', inverse_of: :tickets
   belongs_to :project
 
-  has_one_attached :screen_shot
+  has_one_attached :screen_shot, content_type: %i[png gif]
+
+  validates :title, :type, :status, presence: true
+  validates :title, uniqueness: { scope: :project }
 end

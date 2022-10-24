725c12a5199446744f310080aa6d742252e8cb9d
yousamaaa <usama.yousaf@devsinc.com>
2022-10-24 05:43:56 +0500

    QA ASSOCIATION ADDED

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,7 +3,7 @@
 class Ticket < ApplicationRecord
   enum type: { Bug: 'Bug', Feature: 'Feature' }
 
-  belongs_to :user,       foreign_key: 'creator_id', inverse_of: :tickets, optional: true
+  belongs_to :qa,         foreign_key: 'creator_id', inverse_of: :tickets, optional: true
   belongs_to :developer,  class_name: 'User', foreign_key: 'assigned_to_id', inverse_of: :tickets, optional: true
   belongs_to :project
 

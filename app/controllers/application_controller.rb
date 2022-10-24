6e179507cb8d6e87d32689146678818cba2472d1
yousamaaa <usama.yousaf@devsinc.com>
2022-10-24 05:17:55 +0500

    PUNDIT GEM INSTALLED
    * Pundit gem added to gemfile
    * Included in application controller file
    * Generated policies folder and application policy file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  include Pundit::Authorization
+  
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
